c3ae3e27737bee4a10bb364f139f3d35255ce940
JohnSommerOsterlin <90193135+JohnSommerOsterlin@users.noreply.github.com>
2022-05-24 11:15:21 +0200

    changing week in DOM working

--- public/script/script.js
+++ public/script/script.js
@@ -1,29 +1,76 @@
 const changeWeek = document.querySelector(".change-week-container");
 let weekNumber = document.getElementById("week-number");
+let weekDates = document.querySelector(".week-dates");
 
 
 // function addEventBtn() {
 //     console.log("click");
 // }
 
-
 // console.log(changeWeek);
 
-changeWeek.addEventListener("click", function (event) {
-    let target = event.target;
-    // console.log(target);
+let date = new Date();
+let yyyymmdd = date.toLocaleDateString();
+let dayOfWeek = date.getDate();
 
-    if (target.id === "scroll-left") {
 
-        console.log("clicked left");
-    } else if (target.id === "scroll-right") {
-        let thisWeek = Number(weekNumber.innerText)
-        let nextWeek = Math.min(thisWeek + 1, 52)
-        firstDayOfWeek()
-        console.log("clicked right");
+function firstDayOfWeek(date) {
+    let d = new Date(date);
+    let dayOfWeek = d.getDay();
+    let preDays = -dayOfWeek;
+    let postDays = 7 + preDays;
+    let dates = [];
+    d.setDate(d.getDate() + preDays);
+    for (let i = preDays; i < postDays; i++) {
+        d.setDate(d.getDate() + 1);
+        let yyyymmdd = d.toLocaleDateString();
+        console.log("yyyymmdd", yyyymmdd);
+        let obj = {
+            date: yyyymmdd,
+            dd: "något..."
+        }
+        dates.push(obj);
     }
-    getWeekNumber()
-});
+    return dates;
+};
+firstDayOfWeek()
+
+let calendarDates = firstDayOfWeek(date);
+
+
+function showWeekDates(dates) {
+    // radera alla li element
+    weekDates.innerText = "";
+    // dateInput.innerText = "";
+    dates.forEach(element => {
+        // console.log("element.date", element.date);
+
+        // skapa ett list element
+        let div = document.createElement("div");
+        div.classList.add("dayCard")
+        let pDate = document.createElement("p");
+        pDate.classList.add("date")
+        pDate.id = element.date
+
+        // ange innehåll
+        pDate.innerText = element.date;
+
+        // lägg till det omslutande ul elementet
+        // Lägg till månad
+        // let pMonth = document.createElement("p");
+        // pMonth.innerText = months[element.date.slice(5, 7) - 1]
+
+        // Lägg till dag
+        // let pDay = document.createElement("p");
+        // pDay.innerText = weekDays[3]
+        weekDates.appendChild(div);
+        // div.appendChild(pDay);
+        div.appendChild(pDate);
+        // div.appendChild(pMonth);
+    });
+};
+
+showWeekDates(calendarDates);
 
 function getWeekNumber() {
     let currentDate = new Date();
@@ -31,28 +78,95 @@ function getWeekNumber() {
     let numberOfDays = Math.floor((currentDate - oneJan) / (24 * 60 * 60 * 1000));
     let result = Math.ceil((currentDate.getDay() + numberOfDays) / 7);
     console.log(result);
-    weekNumber.innerHTML = 
-    `
+    weekNumber.innerHTML =
+        `
         <h3>Week ${result}</h3>
     `
-}
-getWeekNumber()
-
-
-
-let today = new Date();
-// --> Date function <-- //
-function firstDayOfWeek(date) {
-    for (let i = 0; i < 7; i++) {
-        let firstDay = today.getDate() - (today.getDay()) + i
-        let date = new Date(today.setDate(firstDay + 1)).toLocaleDateString();
-        console.log(date);
-        document.getElementById("date" + i).textContent = date;
-    }
 };
-firstDayOfWeek()
 
 
+
+
+// let today = new Date();
+
+// --> Date function <-- //
+
+function getWeekNumber() {
+    let currentdate = new Date();
+    let oneJan = new Date(currentdate.getFullYear(), 0, 1);
+    let numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000));
+    let result = Math.ceil((currentdate.getDay() + numberOfDays) / 7); // Tar jag bort -1 blir veckan rätt
+    // console.log(`The week number of the current date (${currentdate}) is ${result}.`);
+    weekNumber.innerHTML = `
+        <h3> Week <span id="weekNumber">${result}</span> </h3>
+    `;
+        changeWeek.addEventListener("click", function (event) {
+            let target = event.target;
+            if (target.id === "scroll-left") {
+                console.log("klick vänster");
+
+                // kontrollera vilken dag som nu är veckans första dag
+                let currentFirstDayOfWeekElement = document.querySelector(".date");
+                // console.log("dateFirstDayOfWeekElement", currentFirstDayOfWeekElement);
+
+                let currentMonday = currentFirstDayOfWeekElement.textContent;
+                // console.log("currentMonday", currentMonday);
+
+                // en vecka tidigare...
+                // skapa först ett äkta datumobjekt av currentMonday
+                let day = new Date(currentMonday);
+
+                // en vecka tidigare
+                day.setDate(day.getDate() - 7);
+                // console.log("day...", day);
+
+                // ändra innehållet i ul elementet
+                // hämta först veckans datum
+                calendarDates = firstDayOfWeek(day);
+                // console.log("calendarDates", calendarDates);
+                weekNumber.innerHTML = `
+                <h3> Week <span id="weekNumber">${result -= 1}</span> </h3>
+                `;
+
+                showWeekDates(calendarDates);
+                // console.log(nextWeek)
+                // console.log(previousWeek);
+                
+
+            } else if (target.id === "scroll-right") {
+                console.log("Klick höger");
+
+                // kontrollera vilken dag som nu är veckans första dag
+                let currentFirstDayOfWeekElement = document.querySelector(".date");
+                // console.log("dateFirstDayOfWeekElement", currentFirstDayOfWeekElement);
+
+                let currentMonday = currentFirstDayOfWeekElement.textContent;
+                // console.log("currentMonday", currentMonday);
+
+                // en vecka tidigare...
+                // skapa först ett äkta datumobjekt av currentMonday
+                let day = new Date(currentMonday);
+
+                // en vecka tidigare
+                day.setDate(day.getDate() + 7);
+                // console.log("day...", day);
+
+                // ändra innehållet i ul elementet
+                // hämta först veckans datum
+                calendarDates = firstDayOfWeek(day);
+                // console.log("calendarDates", calendarDates);
+
+                weekNumber.innerHTML =
+                `
+                <h3> Week <span id="weekNumber">${result += 1}</span> </h3>
+                `
+
+                showWeekDates(calendarDates);
+                // console.log(nextWeek)
+            }
+        });
+};
+getWeekNumber()
 
 
 async function handleDelete(id) {
@@ -64,6 +178,8 @@ async function handleDelete(id) {
         window.location.href = response.url;
     }
 };
+
+
 
 async function handleEdit(evt) {
     const id = Number(evt.target.dataset.id); // data-id -> dataset.id
